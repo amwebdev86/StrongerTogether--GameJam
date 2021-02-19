75bccd6f661880f31f6e0f0f097e2fb9cbed451b
Auroiah Morgan <amwebdev@outlook.com>
2021-02-19 15:07:51 -0500

    Updates Enemy Death

--- Assets/SPACE/Scripts/Enemy/EnemyHealth.cs
+++ Assets/SPACE/Scripts/Enemy/EnemyHealth.cs
@@ -1,32 +1,39 @@
 ﻿
+using System.Collections;
 using UnityEngine;
 namespace SPACE.Enemy
 {
 
-    public class EnemyHealth : MonoBehaviour
+  public class EnemyHealth : MonoBehaviour
+  {
+    [SerializeField] float health = 1;
+
+    public void DamageEnemy(float amount)
     {
-        [SerializeField] float health = 1;
+      health -= amount;
+      if (health < 0)
+      {
+        health = 0;
+        StartCoroutine(EnemyDeath());
 
-        public void DamageEnemy(float amount)
-        {
-            health -= amount;
-            if (health < 0)
-            {
-                health = 0;
+      }
 
-                //TODO: Call death animation.
-                Destroy(gameObject);
-            }
+    }
 
-        }
-        /// <summary>
-        /// Sent when another object enters a trigger collider attached to this
-        /// object (2D physics only).
-        /// </summary>
-        /// <param name="other">The other Collider2D involved in this collision.</param>
+    IEnumerator EnemyDeath()
+    {
+      Transform enemyTransform = GetComponent<Transform>();
+      enemyTransform.Rotate(new Vector3(0, 0, -90));
+      yield return new WaitForSeconds(1f);
+      Destroy(gameObject);
 
 
 
     }
+
+
+
+
+  }
 
 }
