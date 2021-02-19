fa4015640d2801616d517b03678d4cd7f795e023
Auroiah Morgan <amwebdev@outlook.com>
2021-02-19 15:17:04 -0500

    Updates Player Death

--- Assets/SPACE/Scripts/Player/PlayerHealth.cs
+++ Assets/SPACE/Scripts/Player/PlayerHealth.cs
@@ -4,68 +4,66 @@ using UnityEngine;
 
 namespace SPACE.Player
 {
-    /// <summary>
-    /// Controls the player's health.
-    /// </summary>
-    public class PlayerHealth : MonoBehaviour
+  /// <summary>
+  /// Controls the player's health.
+  /// </summary>
+  public class PlayerHealth : MonoBehaviour
+  {
+    [SerializeField]
+    int _maxHealth;
+    public int MaxHealth
     {
-        [SerializeField]
-        int _maxHealth;
-        public int MaxHealth
-        {
-            get
-            {
-                return _maxHealth;
-            }
-        }
-        int _currentHealth;
-        public int CurrentHealth
-        {
-            get
-            {
-                return _currentHealth;
-            }
-        }
-        [SerializeField]
-        //HealthBar _healthBar;
-        private void Start()
-        {
-            _currentHealth = _maxHealth;
-            //_healthBar.SetMaxHealth(_maxHealth);
-        }
-        /// <summary>
-        /// Damages the player.
-        /// </summary>
-        /// <param name="damage">Amount to damage player</param>
-        public void TakeDamage(int damage)
-        {
-
-            _currentHealth -= damage;
-            //_healthBar.SetHealth(_currentHealth);
-            GameManager.Instance.UpdateHealthBar(_currentHealth);
-            if (_currentHealth <= 0)
-            {
-                //TODO: Invoke Gameover event
-
-                _currentHealth = 0;
-                GameManager.Instance.m_GameOverEvent.Invoke();
-
-            }
-            Debug.Log($"Player was damaged {_currentHealth}-{damage}");
-        }
-        /// <summary>
-        /// Heals the Player
-        /// </summary>
-        /// <param name="amount">Amount to heal the player</param>
-        public void Heal(int amount)
-        {
-            _currentHealth += amount;
-            if (_currentHealth > _maxHealth)
-            {
-                _currentHealth = _maxHealth;
-            }
-        }
-
+      get
+      {
+        return _maxHealth;
+      }
     }
+    int _currentHealth;
+    public int CurrentHealth
+    {
+      get
+      {
+        return _currentHealth;
+      }
+    }
+    [SerializeField]
+    //HealthBar _healthBar;
+    private void Start()
+    {
+      _currentHealth = _maxHealth;
+      //_healthBar.SetMaxHealth(_maxHealth);
+    }
+    /// <summary>
+    /// Damages the player.
+    /// </summary>
+    /// <param name="damage">Amount to damage player</param>
+    public void TakeDamage(int damage)
+    {
+
+      _currentHealth -= damage;
+      //_healthBar.SetHealth(_currentHealth);
+      GameManager.Instance.UpdateHealthBar(_currentHealth);
+      if (_currentHealth <= 0)
+      {
+        _currentHealth = 0;
+        GameManager.Instance.m_GameOverEvent.Invoke();
+
+      }
+      Debug.Log($"Player was damaged {_currentHealth}-{damage}");
+    }
+    /// <summary>
+    /// Heals the Player
+    /// </summary>
+    /// <param name="amount">Amount to heal the player</param>
+    public void Heal(int amount)
+    {
+      _currentHealth += amount;
+      if (_currentHealth > _maxHealth)
+      {
+        _currentHealth = _maxHealth;
+      }
+    }
+
+  }
 
 }
