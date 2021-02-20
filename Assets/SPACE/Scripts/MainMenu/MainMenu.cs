be08d34224c38ab864f7bc2371ec3a456dbea062
Auroiah Morgan <amwebdev@outlook.com>
2021-02-20 06:22:18 -0500

    Adds Quit functionality and pause menu

--- Assets/SPACE/Scripts/MainMenu/MainMenu.cs
+++ Assets/SPACE/Scripts/MainMenu/MainMenu.cs
@@ -1,37 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 namespace SPACE.MainMenu
 {
-    public class MainMenu : MonoBehaviour
+  public class MainMenu : MonoBehaviour
+  {
+
+    public void StartGame()
     {
-        [SerializeField]
-        TMP_Text title;
-
-
-        private void Start()
-        {
-            StartCoroutine(TitleGlow());
-        }
-
-        public void StartGame()
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-        IEnumerator TitleGlow()
-        {
-            while(SceneManager.GetActiveScene().name == "MainMenu")
-            {
-                title.fontSharedMaterial.SetFloat(ShaderUtilities.ID_GlowPower, 1);
-                yield return new WaitForSeconds(5);
-                title.fontSharedMaterial.SetFloat(ShaderUtilities.ID_GlowPower, .75f);
-                yield return new WaitForSeconds(8);
-                title.fontSharedMaterial.SetFloat(ShaderUtilities.ID_GlowPower, .5f);
-
-
-            }
-        }
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame(){
+      Application.Quit();
+    }
+
+  }
 }
